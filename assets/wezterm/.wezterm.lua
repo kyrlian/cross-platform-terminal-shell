69c760876a9fc2446f3fbd4e11175a90e1eed017
Rémi Serra <remi.serra@fr.ibm.com>
2024-05-21 21:39:48 +0200

    added zoom and resize panels

--- assets/wezterm/.wezterm.lua
+++ assets/wezterm/.wezterm.lua
@@ -19,15 +19,20 @@ config.default_prog = { '/opt/homebrew/bin/nu', '-l' }
 -- Set tmux like bindings
 config.leader = { key = "w", mods = "CTRL" }
 config.keys = {
-    { key = "%", mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
-    { key = "-", mods = "LEADER", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
-    { key = "s", mods = "LEADER", action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
-    { key = "s", mods = "LEADER|CTRL", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
-    { key = "x", mods = "LEADER", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
-    { key = "LeftArrow", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Left"}},
-    { key = "DownArrow", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Down"}},
-    { key = "UpArrow", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Up"}},
-    { key = "RightArrow", mods = "LEADER",       action=wezterm.action{ActivatePaneDirection="Right"}},
+    { key = "%",          mods = "LEADER",      action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+    { key = "-",          mods = "LEADER",      action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+    { key = "s",          mods = "LEADER",      action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } } },
+    { key = "s",          mods = "LEADER|CTRL", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
+    { key = "x",          mods = "LEADER",      action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+    { key = "Space",      mods = "LEADER",      action = "TogglePaneZoomState" },
+    { key = "LeftArrow",  mods = "LEADER",      action = wezterm.action { ActivatePaneDirection = "Left" } },
+    { key = "DownArrow",  mods = "LEADER",      action = wezterm.action { ActivatePaneDirection = "Down" } },
+    { key = "UpArrow",    mods = "LEADER",      action = wezterm.action { ActivatePaneDirection = "Up" } },
+    { key = "RightArrow", mods = "LEADER",      action = wezterm.action { ActivatePaneDirection = "Right" } },
+    { key = "LeftArrow",  mods = "LEADER|CTRL", action = wezterm.action { AdjustPaneSize = { "Left", 5 } } },
+    { key = "DownArrow",  mods = "LEADER|CTRL", action = wezterm.action { AdjustPaneSize = { "Down", 5 } } },
+    { key = "UpArrow",    mods = "LEADER|CTRL", action = wezterm.action { AdjustPaneSize = { "Up", 5 } } },
+    { key = "RightArrow", mods = "LEADER|CTRL", action = wezterm.action { AdjustPaneSize = { "Right", 5 } } },
 }
 
 -- Return the configuration to wezterm
