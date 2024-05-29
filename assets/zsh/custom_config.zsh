a03ddfdfe221c1224beee7ac2fe7fda662ff1243
kyrlian <kyrlian@gmail.com>
2024-05-29 20:07:55 +0200

    init zsh banner

---
+++ assets/zsh/custom_config.zsh
@@ -0,0 +1,35 @@
+# Sourced by  ~/.zshrc
+
+# Color support : $fg[red]
+# https://stackoverflow.com/questions/689765/how-can-i-change-the-color-of-my-prompt-in-zsh-different-from-normal-text
+autoload -U colors && colors
+
+#############
+## Banner  ##
+#############
+banner() { 
+    print "welcome to Zsh"
+    print "Date : $(date +'%Y-%m-%d %H:%M:%S')"
+}
+banner
+
+##############
+## Aliases  ##
+##############
+
+#python
+# alias venvc='python3 -m venv venv'
+alias venvc='virtualenv virtualenv'
+alias activate='source virtualenv/bin/activate'
+
+##############
+## Prompt   ##
+##############
+
+# Starship prompt
+eval "$(starship init zsh)"
+
+###############
+## Feedback  ##
+###############
+echo "$fg[cyan]Zsh custom profile loaded$reset_color"
