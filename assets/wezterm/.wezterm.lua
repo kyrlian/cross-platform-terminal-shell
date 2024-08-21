a0ab1ba34e2d88df492c9f1ef685f3d47f1d4972
Remi Serra <remi.serra@gmail.com>
2024-08-21 14:58:09 +0200

    need hard path

--- assets/wezterm/.wezterm.lua
+++ assets/wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
--- lcation  ~/.wezterm.lua
+-- location  ~/.wezterm.lua
 -- load custom wezterm_config
 --- win
 -- dofile('E:/xxxxx/assets/wezterm/wezterm_config.lua')
 --- mac
--- dofile('~/Documents/xxxx/assets/wezterm/wezterm_config.lua')
+-- dofile('/Users/me/Documents/xxxx/assets/wezterm/wezterm_config.lua')
